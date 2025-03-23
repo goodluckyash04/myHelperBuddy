c72102cfb69fe59af9c56f26e8657b1883d6622a
yash <yash.goodluck4@gmail.com>
2025-03-23 17:10:38 +0530

    db_updated

--- accounts/management/commands/backup_db.py
+++ accounts/management/commands/backup_db.py
@@ -48,7 +48,7 @@ class Command(BaseCommand):
             self.send_todays_task_reminder()
 
             with open(settings.JSON_DB) as file:
-                last_backup = json.loads(file.read()).get("last_backup")
+                last_backup = json.loads(file.read()).get("last_backup") if file.read() else None
 
             last_backup = (
                 datetime.datetime.strptime(last_backup, "%Y-%m-%d %H:%M:%S")
